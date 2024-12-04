528d5b1b65b70a5a860ce8cdde10737a19440992
bakbakkie <1006inseo@naver.com>
2024-12-04 13:36:49 +0900

    Add files via upload

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 # pip install langchain-openai
 # pip intsall streamlit
 
-from dotenv import load_dotenv
-load_dotenv(override=True)
+# from dotenv import load_dotenv
+# load_dotenv(override=True)
 
 from langchain_openai import ChatOpenAI
 chat_model = ChatOpenAI()
